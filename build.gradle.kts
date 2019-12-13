0574f22fc95734f972238a657d7ffd264c2a6591
Sergio Martinez <smartinez@brightcove.com>
2019-12-13 15:27:03 -0700

    Converted gradle scripts to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    extra.apply {
+        set("kotlin_version","1.3.50")
+    }
+
+    repositories {
+        maven { url = uri("https://jitpack.io") }
+        google()
+        jcenter()
+
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
+        
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
